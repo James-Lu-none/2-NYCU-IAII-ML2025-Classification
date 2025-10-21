1d9fcf63f5ee75f6fd8e2ba6cfb819eb1a13a553
James-Lu-none <jamesabcde277@gmail.com>
2025-10-21 19:38:24 +0800

    add models.py

---
+++ models.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+from torchvision import models
+
+num_classes = 50
+def resnet50_v1():
+    model = models.resnet50(pretrained=True)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    return model
+
+def resnet50_v2():
+    model = models.resnet50(pretrained=True)
+    model.fc = nn.Sequential(
+        nn.Linear(model.fc.in_features, 512),
+        nn.ReLU(),
+        nn.Dropout(0.3),
+        nn.Linear(512, num_classes)
+    )
+
+def resnet101_v1():
+    model = models.resnet50(pretrained=True)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    return model
