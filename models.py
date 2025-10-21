ec8278c03ce4f4e9cbc5784a5f50b65e1f31cab4
James-Lu-none <jamesabcde277@gmail.com>
2025-10-21 20:33:42 +0800

    update model loading to use 'weights=DEFAULT' in resnet functions

--- models.py
+++ models.py
@@ -3,12 +3,12 @@ from torchvision import models
 
 num_classes = 50
 def resnet50_v1():
-    model = models.resnet50(pretrained=True)
+    model = models.resnet50(weights='DEFAULT')
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
 
 def resnet50_v2():
-    model = models.resnet50(pretrained=True)
+    model = models.resnet50(weights='DEFAULT')
     model.fc = nn.Sequential(
         nn.Linear(model.fc.in_features, 512),
         nn.ReLU(),
@@ -17,6 +17,6 @@ def resnet50_v2():
     )
 
 def resnet101_v1():
-    model = models.resnet50(pretrained=True)
+    model = models.resnet50(weights='DEFAULT')
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
