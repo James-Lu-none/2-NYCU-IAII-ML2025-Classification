e3835e21fa11f039456988590f9c446327259bd1
James-Lu-none <jamesabcde277@gmail.com>
2025-10-22 23:19:56 +0800

    refactor: enhance image augmentation pipeline with increased probabilities and multiple augmented outputs

--- preprocess.py
+++ preprocess.py
@@ -81,39 +81,41 @@ class AddSaltPepperNoise(object):
         tensor[noise < self.salt_prob] = 1  # Salt noise: setting some pixels to 1
         tensor[noise > 1 - self.pepper_prob] = 0  # Pepper noise: setting some pixels to 0
         return tensor
+
 transform = T.Compose([
     T.ToTensor(),  # Convert PIL image to tensor
 
-    T.RandomApply([T.RandomHorizontalFlip()], p=0.1),
-    T.RandomApply([T.RandomVerticalFlip()], p=0.1),
-    T.RandomApply([T.RandomRotation(10)], p=0.1),
+    T.RandomApply([T.RandomHorizontalFlip()], p=0.2),
+    T.RandomApply([T.RandomVerticalFlip()], p=0.2),
+    T.RandomApply([T.RandomRotation(10)], p=0.2),
 
-    T.RandomApply([T.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)], p=0.1),
-    T.RandomGrayscale(p=0.1),
-    T.RandomInvert(p=0.1),
-    T.RandomPosterize(bits=2, p=0.1),
+    T.RandomApply([T.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)], p=0.2),
+    T.RandomGrayscale(p=0.2),
+    T.RandomInvert(p=0.2),
+    T.RandomPosterize(bits=2, p=0.2),
     T.RandomApply([T.RandomSolarize(threshold=1.0)], p=0.05),
-    T.RandomApply([T.RandomAdjustSharpness(sharpness_factor=2)], p=0.1),
+    T.RandomApply([T.RandomAdjustSharpness(sharpness_factor=2)], p=0.2),
 
-    T.RandomApply([AddGaussianNoise(0., 0.05)], p=0.1),  # mean and std
-    T.RandomApply([AddPoissonNoise(lam=0.1)], p=0.1),  # mean and std
-    T.RandomApply([AddSpeckleNoise(noise_level=0.1)], p=0.1),
-    T.RandomApply([AddSaltPepperNoise(salt_prob=0.05, pepper_prob=0.05)], p=0.1),
+    T.RandomApply([AddGaussianNoise(0., 0.05)], p=0.2),  # mean and std
+    T.RandomApply([AddPoissonNoise(lam=0.1)], p=0.2),  # mean and std
+    T.RandomApply([AddSpeckleNoise(noise_level=0.1)], p=0.2),
+    T.RandomApply([AddSaltPepperNoise(salt_prob=0.05, pepper_prob=0.05)], p=0.2),
 
-    T.RandomApply([T.RandomPerspective(distortion_scale=0.6, p=1.0)], p=0.1),
-    T.RandomApply([T.RandomAffine(degrees=(30, 70), translate=(0.1, 0.3), scale=(0.5, 0.75))], p=0.1),
-    T.RandomApply([T.ElasticTransform(alpha=250.0)], p=0.1),
+    T.RandomApply([T.RandomPerspective(distortion_scale=0.6, p=1.0)], p=0.2),
+    T.RandomApply([T.RandomAffine(degrees=(30, 70), translate=(0.1, 0.3), scale=(0.5, 0.75))], p=0.2),
+    T.RandomApply([T.ElasticTransform(alpha=250.0)], p=0.2),
 
-    T.RandomApply([T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5.))], p=0.1),
+    T.RandomApply([T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5.))], p=0.2),
 
     T.RandomApply([AddGaussianNoise(0., 0.001)], p=1.0),  # mean and std
     T.ToPILImage()  # Convert tensor back to PIL image for saving
-
 ])
 
 if not os.path.exists(dst_folder):
     os.makedirs(dst_folder)
 
+n_per_class = 10000
+transform_per_image = 2
 for c in os.listdir(src_folder):
     i = 0
     src_class_folder = os.path.join(src_folder, c)
@@ -121,11 +123,20 @@ for c in os.listdir(src_folder):
     os.makedirs(dst_class_folder, exist_ok=True)
     print(f"{len(os.listdir(src_class_folder))} files in class: {c}")
     for f in os.listdir(src_class_folder):
+        if i >= n_per_class:
+            break
         src_path = os.path.join(src_class_folder, f)
         img = Image.open(src_path).convert("RGB")
-        aug_img = transform(img)
-        dst_path = os.path.join(dst_class_folder, f"{i}.jpg")
-        aug_img.save(dst_path)
+        aug_imgs = []
+        for k in range(transform_per_image):
+            aug_img = transform(img)
+            aug_imgs.append(aug_img)
+
+        # before_path = os.path.join(dst_class_folder, f"{i}.jpg")
+        # img.save(before_path)
+        for j, aug_img in enumerate(aug_imgs):
+            dst_path = os.path.join(dst_class_folder, f"{i}_{j}.jpg")
+            aug_img.save(dst_path)
         i += 1
-    print(f"{i} files generated")
+    print(f"{i*transform_per_image} file generated")
 print("Image augmentation completed.")
